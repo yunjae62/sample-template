cbbbc82cf77b1e961e8668106b46585eddc747c6
yunjae62 <wodbs7758@naver.com>
2025-04-11 15:36:32 +0900

    refactor: 공통 응답 Record 변경

--- src/main/java/ex/sample/global/common/response/CommonResponse.java
+++ src/main/java/ex/sample/global/common/response/CommonResponse.java
@@ -1,63 +1,43 @@
 package ex.sample.global.common.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-@Builder(access = AccessLevel.PRIVATE)
-public class CommonResponse<T> {
-
+public record CommonResponse<T>(
     @Schema(description = "응답 코드", example = "0")
-    private Integer code; // 커스텀 응답 코드
+    Integer code, // 커스텀 응답 코드
+
     @Schema(description = "응답 메시지", example = "정상 처리 되었습니다.")
-    private String message; // 응답에 대한 설명
+    String message, // 응답에 대한 설명
+
     @Schema(description = "응답 데이터", example = "{}")
-    private T data; // 응답에 필요한 데이터
+    T data // 응답에 필요한 데이터
+) {
 
     /**
      * data 필드에 값을 넣을 때 사용하는 메서드 - data 필드가 필요 없는 경우
      */
     public static CommonResponse<CommonEmptyRes> success() {
-        return getSuccessRes(new CommonEmptyRes());
+        return new CommonResponse<>(0, "정상 처리 되었습니다.", new CommonEmptyRes());
     }
 
     /**
      * data 필드에 값을 넣을 때 사용하는 메서드 - data 필드가 필요한 경우
      */
     public static <T> CommonResponse<T> success(T data) {
-        return getSuccessRes(data);
-    }
-
-    private static <T> CommonResponse<T> getSuccessRes(T data) {
-        return CommonResponse.<T>builder()
-            .code(0)
-            .message("정상 처리 되었습니다.")
-            .data(data)
-            .build();
+        return new CommonResponse<>(0, "정상 처리 되었습니다.", data);
     }
 
     /**
      * 에러 발생 시 특정 에러에 맞는 응답하는 메서드 - data 필드가 필요 없는 경우
      */
     public static CommonResponse<CommonEmptyRes> error(ErrorCase errorCase) {
-
-        return CommonResponse.<CommonEmptyRes>builder()
-            .code(errorCase.getCode())
-            .message(errorCase.getMessage())
-            .data(new CommonEmptyRes())
-            .build();
+        return new CommonResponse<>(errorCase.getCode(), errorCase.getMessage(), new CommonEmptyRes());
     }
 
     /**
      * 에러 발생 시 특정 에러에 맞는 응답하는 메서드 - data 필드가 필요한 경우
      */
     public static <T> CommonResponse<T> error(ErrorCase errorCase, T data) {
-        return CommonResponse.<T>builder()
-            .code(errorCase.getCode())
-            .message(errorCase.getMessage())
-            .data(data)
-            .build();
+        return new CommonResponse<>(errorCase.getCode(), errorCase.getMessage(), data);
     }
 }
