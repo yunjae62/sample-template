9eef4cf121d64efbf428584efe0737dd9548b18f
yunjae62 <wodbs7758@naver.com>
2025-04-13 01:17:57 +0900

    feat: property 테스트 개선

--- src/test/java/ex/sample/domain/sample/property/SamplePropertyTest.java
+++ src/test/java/ex/sample/domain/sample/property/SamplePropertyTest.java
@@ -2,27 +2,32 @@ package ex.sample.domain.sample.property;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
-@SpringBootTest
 class SamplePropertyTest {
 
-    @Autowired
-    private SampleProperty sampleProperty;
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+        .withUserConfiguration(TestConfig.class)
+        .withPropertyValues(
+            "sample.api.url=http://localhost:8080/samples/properties",
+            "sample.api.key=SAMPLE_API_KEY"
+        );
 
-    @DisplayName("SampleProperty Bean 주입 테스트")
     @Test
-    void sample_property_bean_test() {
-        // given
-        String url = "http://localhost:8080/samples/properties" + "?key=" + "SAMPLE_API_KEY";
+    void sampleProperty_test() {
+        contextRunner.run(context -> {
+            SampleProperty property = context.getBean(SampleProperty.class);
 
-        // when
-        String samplePropertyUrl = sampleProperty.getUrlWithKey();
+            assertThat(property.url()).isEqualTo("http://localhost:8080/samples/properties");
+            assertThat(property.key()).isEqualTo("SAMPLE_API_KEY");
+            assertThat(property.getUrlWithKey()).isEqualTo("http://localhost:8080/samples/properties?key=SAMPLE_API_KEY");
+        });
+    }
 
-        // then
-        assertThat(samplePropertyUrl).isEqualTo(url);
+    @EnableConfigurationProperties(SampleProperty.class)
+    static class TestConfig {
+
     }
 }
