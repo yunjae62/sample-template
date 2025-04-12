50068c8372516da6ba3c8b23efe2f642ec8ee609
yunjae62 <wodbs7758@naver.com>
2025-04-12 23:01:14 +0900

    feat: AuthenticationProvider 추가

---
+++ src/main/java/ex/sample/global/security/provider/AccessTokenAuthenticationProvider.java
@@ -0,0 +1,69 @@
+package ex.sample.global.security.provider;
+
+import ex.sample.global.exception.GlobalException;
+import ex.sample.global.jwt.JwtStatus;
+import ex.sample.global.redis.RedisUtil2;
+import ex.sample.global.response.ResponseCode;
+import ex.sample.global.security.authentication.AccessTokenAuthentication;
+import ex.sample.global.security.jwt.JwtBearerUtils;
+import ex.sample.global.security.jwt.JwtClaimsExtractor;
+import ex.sample.global.security.jwt.JwtValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AccessTokenAuthenticationProvider implements AuthenticationProvider {
+
+    private final RedisUtil2 redisUtil;
+    private final JwtValidator jwtValidator;
+    private final JwtClaimsExtractor jwtClaimsExtractor;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String token = getTokenWithoutBearer(authentication);
+
+        validateTokenStatus(token);
+
+        String subject = jwtClaimsExtractor.getSubject(token);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(subject);
+
+        // 로그아웃 여부 확인 후 예외처리
+        validateLogout(subject);
+
+        return new AccessTokenAuthentication(userDetails, token, userDetails.getAuthorities());
+    }
+
+    private String getTokenWithoutBearer(Authentication authentication) {
+        String tokenWithBearer = (String) authentication.getCredentials();
+        return JwtBearerUtils.removePrefix(tokenWithBearer);
+    }
+
+    private void validateTokenStatus(String token) {
+        JwtStatus tokenStatus = jwtValidator.validate(token);
+
+        if (tokenStatus.equals(JwtStatus.INVALID)) {
+            throw new GlobalException(ResponseCode.INVALID_TOKEN);
+        }
+
+        if (tokenStatus.equals(JwtStatus.EXPIRED)) {
+            throw new GlobalException(ResponseCode.EXPIRED_ACCESS_TOKEN);
+        }
+    }
+
+    private void validateLogout(String subject) {
+        redisUtil.get(subject, String.class)
+            .orElseThrow(() -> new GlobalException(ResponseCode.LOGIN_REQUIRED));
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return AccessTokenAuthentication.class.isAssignableFrom(authentication);
+    }
+}
---
+++ src/main/java/ex/sample/global/security/provider/UsernamePasswordAuthenticationProvider.java
@@ -0,0 +1,30 @@
+package ex.sample.global.security.provider;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UsernamePasswordAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = (String) authentication.getPrincipal();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        return new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
