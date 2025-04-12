72ccc1ed12ce35b9158aa69de6e20e2535aa9774
yunjae62 <wodbs7758@naver.com>
2025-04-12 23:49:44 +0900

    feat: 로그인 DTO 추가

--- src/main/java/ex/sample/global/security/filter/LoginFilter.java
+++ src/main/java/ex/sample/global/security/filter/LoginFilter.java
@@ -7,6 +7,9 @@ import ex.sample.global.security.WebSecurityConfig;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +19,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Slf4j(topic = "LoginFilter")
+@Slf4j
 @RequiredArgsConstructor
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -50,5 +53,16 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
         log.info("login failed message : {}", failed.getMessage());
         throw new GlobalException(ResponseCode.NOT_FOUND);
+    }
+
+    public record UserLoginReq(
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        String email,
+
+        @NotBlank(message = "비밀번호는 공백이 될 수 없습니다.")
+        @Size(min = 8, max = 60, message = "비밀번호의 글자수는 8자 이상 60자 이하입니다.")
+        String password
+    ) {
+
     }
 }
