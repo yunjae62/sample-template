cc06f099ae66bc651cf6542e28f018394ea228b4
yunjae62 <wodbs7758@naver.com>
2025-07-26 19:17:06 +0900

    feat: Money 값객체에 통화 개념 추가

--- src/main/java/ex/sample/domain/model/Money.java
+++ src/main/java/ex/sample/domain/model/Money.java
@@ -1,6 +1,7 @@
 package ex.sample.domain.model;
 
 import jakarta.persistence.Embeddable;
+import java.math.BigDecimal;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,26 +13,60 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 스펙상 엔티티나 임베디드 타입은 기본 생성자가 필수
 public class Money {
 
-    private Long amount;
+    public static final Money ZERO_KRW = Money.ofKRW(BigDecimal.ZERO);
+    public static final Money ONE_KRW = Money.ofKRW(BigDecimal.ONE);
 
-    public Money(long amount) {
+    public static final Money ZERO_USD = Money.ofUSD(BigDecimal.ZERO);
+    public static final Money ONE_USD = Money.ofUSD(BigDecimal.ONE);
+
+    private BigDecimal amount;
+    private Currency currency;
+
+    public Money(BigDecimal amount, Currency currency) {
+        if (amount == null || currency == null) {
+            throw new NullPointerException("금액과 통화는 널이 될 수 없습니다.");
+        }
         this.amount = amount;
+        this.currency = currency;
     }
 
-    public Money add(Money money) {
-        return new Money(this.amount + money.amount);
+    public static Money of(BigDecimal amount, Currency currency) {
+        return new Money(amount, currency);
     }
 
-    public Money subtract(final Money money) {
-        return new Money(this.amount - money.amount);
+    public static Money ofKRW(BigDecimal amount) {
+        return of(amount, Currency.KRW);
     }
 
-    public Money multiply(int multiplier) {
-        return new Money(this.amount * multiplier);
+    public static Money ofUSD(BigDecimal amount) {
+        return of(amount, Currency.USD);
+    }
+
+    public Money add(Money other) {
+        checkCurrencyMatch(other);
+        return Money.of(this.amount.add(other.amount), this.currency);
+    }
+
+    public Money subtract(Money other) {
+        checkCurrencyMatch(other);
+        return Money.of(this.amount.subtract(other.amount), this.currency);
+    }
+
+    public Money multiply(BigDecimal multiplier) {
+        return Money.of(this.amount.multiply(multiplier), this.currency);
+    }
+
+    private void checkCurrencyMatch(Money other) {
+        if (other == null) {
+            throw new NullPointerException("금액과 통화는 널이 될 수 없습니다.");
+        }
+        if (!this.currency.equals(other.currency)) {
+            throw new IllegalArgumentException("통화가 다릅니다.");
+        }
     }
 
     @Override
     public String toString() {
-        return String.valueOf(amount);
+        return amount.toPlainString() + " " + currency;
     }
 }
