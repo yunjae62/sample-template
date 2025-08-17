71464cbc02d059213abb179d487d8025c017f789
yunjae62 <wodbs7758@naver.com>
2025-08-17 15:22:49 +0900

    feat: 로그인 인증에 비밀번호 검증 절차 추가

--- src/main/java/ex/sample/global/security/provider/UsernamePasswordAuthenticationProvider.java
+++ src/main/java/ex/sample/global/security/provider/UsernamePasswordAuthenticationProvider.java
@@ -1,5 +1,7 @@
 package ex.sample.global.security.provider;
 
+import ex.sample.global.exception.GlobalException;
+import ex.sample.global.response.ResponseCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,18 +9,26 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UsernamePasswordAuthenticationProvider implements AuthenticationProvider {
 
+    private final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = (String) authentication.getPrincipal();
+        String password = (String) authentication.getCredentials();
+
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new GlobalException(ResponseCode.INVALID_CREDENTIALS);
+        }
 
         return new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
     }
