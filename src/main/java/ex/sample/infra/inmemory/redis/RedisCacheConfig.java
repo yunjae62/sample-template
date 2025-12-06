99c7b5b8b6b594abbca508474108be6958db409d
yunjae62 <wodbs7758@naver.com>
2025-12-06 20:50:36 +0900

    build: @Deprecated 예정 코드 수정

--- src/main/java/ex/sample/infra/inmemory/redis/RedisCacheConfig.java
+++ src/main/java/ex/sample/infra/inmemory/redis/RedisCacheConfig.java
@@ -1,15 +1,14 @@
 package ex.sample.infra.inmemory.redis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Duration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import tools.jackson.databind.ObjectMapper;
 
 @Configuration
 public class RedisCacheConfig {
@@ -17,11 +16,9 @@ public class RedisCacheConfig {
     @Bean
     public RedisCacheManager redisCacheManager(ObjectMapper objectMapper, RedisConnectionFactory redisConnectionFactory) {
 
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
-
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig() // 기본 설정
             .serializeKeysWith(SerializationPair.fromSerializer(RedisSerializer.string())) // 키는 문자열
-            .serializeValuesWith(SerializationPair.fromSerializer(serializer)) // 직렬화
+            .serializeValuesWith(SerializationPair.fromSerializer(RedisSerializer.json())) // 직렬화
             .entryTtl(Duration.ofSeconds(5L)) // 기본 캐싱 TTL 설정
             .disableCachingNullValues(); // null 캐싱 불가
 
